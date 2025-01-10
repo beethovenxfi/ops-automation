0f0da5487db6d65f11e43f3d4c09ec091c0eee2e
franz <franzns11@gmail.com>
2025-01-10 17:15:41 +0100

    test

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ async function run(): Promise<void> {
     try {
         const beetsToDistribute: string = core.getInput('beets_to_distribute');
 
-        core.debug(`Distributing ${beetsToDistribute} BEETS next epoch`);
+        core.debug(`DGB: Distributing ${beetsToDistribute} BEETS next epoch`);
+        console.log(`LOG: Distributing ${beetsToDistribute} BEETS next epoch`);
     } catch (error) {
         // Fail the workflow run if an error occurs
         if (error instanceof Error) core.setFailed(error.message);
