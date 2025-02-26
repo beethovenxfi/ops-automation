321d3bb793402e1332c67eac908bcc1eb482bb4d
franz <franzns11@gmail.com>
2025-02-26 18:01:44 +0100

    handle error

--- src/inititalizeRound.ts
+++ src/inititalizeRound.ts
@@ -37,73 +37,76 @@ async function run(): Promise<void> {
 
         await createSnapshot(startTimestamp, endTimestamp, snapshotBlock);
     } catch (error) {
-        if (error instanceof Error) core.setFailed(error.message);
+        console.log(`erroring`);
+        core.setFailed(error as Error);
     }
 }
 
 async function createSnapshot(startTimestamp: number, endTimestamp: number, snapshotBlock: number): Promise<void> {
-    try {
-        // read the vote options from the file
-        const choiceList: GaugeChoice[] = JSON.parse(
-            fs.readFileSync('./gauge-choices/choices.json', 'utf-8'),
-        ) as GaugeChoice[];
-        choiceList.forEach((gauge: GaugeChoice, i: number) => console.log(`${i + 1}) ${gauge.poolName}`));
+    // read the vote options from the file
+    const choiceList: GaugeChoice[] = JSON.parse(
+        fs.readFileSync('./gauge-choices/choices.json', 'utf-8'),
+    ) as GaugeChoice[];
+    choiceList.forEach((gauge: GaugeChoice, i: number) => console.log(`${i + 1}) ${gauge.poolName}`));
 
-        // create snapshot vote
-        console.log('Creating proposal on snapshot');
+    // create snapshot vote
+    console.log('Creating proposal on snapshot');
 
-        // const client = new snapshot.Client712('https://hub.snapshot.org');
-        // const space = 'beets-gauges.eth';
-        const client = new snapshot.Client712(SNAPSHOT_HUB_URL);
-        const space = SNAPSHOT_SPACE;
+    // const client = new snapshot.Client712('https://hub.snapshot.org');
+    // const space = 'beets-gauges.eth';
+    const client = new snapshot.Client712(SNAPSHOT_HUB_URL);
+    const space = SNAPSHOT_SPACE;
 
-        console.log('Start date:', moment.unix(startTimestamp).format('YYYY-MM-DD HH:mm:ss'));
-        console.log('End date:', moment.unix(endTimestamp).format('YYYY-MM-DD HH:mm:ss'));
-        console.log(`Snapshot: ${snapshotBlock}`);
-        console.log(`Space: ${space}`);
+    console.log('Start date:', moment.unix(startTimestamp).format('YYYY-MM-DD HH:mm:ss'));
+    console.log('End date:', moment.unix(endTimestamp).format('YYYY-MM-DD HH:mm:ss'));
+    console.log(`Snapshot: ${snapshotBlock}`);
+    console.log(`Space: ${space}`);
 
-        const gaugeVoteRoundNumber =
-            (startTimestamp - getVoteStartTimestamp(FIRST_GAUGE_VOTE_DAY)) / TWO_WEEKS_IN_SECONDS + 1;
+    const gaugeVoteRoundNumber =
+        (startTimestamp - getVoteStartTimestamp(FIRST_GAUGE_VOTE_DAY)) / TWO_WEEKS_IN_SECONDS + 1;
 
-        const title = `Beets Sonic gauge vote (Round ${gaugeVoteRoundNumber})`;
-        console.log(`Title: ${title}`);
+    const title = `Beets Sonic gauge vote (Round ${gaugeVoteRoundNumber})`;
+    console.log(`Title: ${title}`);
 
-        const body = `This vote decides the distribution of ${
-            process.env.BEETS_TO_DISTRIBUTE
-        } BEETS to gauge emissions for the period of ${moment(process.env.VOTE_END_DAY, 'YYYY-MM-DD')
-            .add(DAYS_DELTA_BETWEEN_VOTE_END_AND_EMISSION_START, 'days')
-            .format('MMMM Do YYYY')} to ${moment(process.env.VOTE_END_DAY, 'YYYY-MM-DD')
-            .add(DAYS_DELTA_BETWEEN_VOTE_END_AND_EMISSION_START, 'days')
-            .add(DAYS_FOR_EMISSIONS, 'days')
-            .format('MMMM Do YYYY')}.
+    const body = `This vote decides the distribution of ${
+        process.env.BEETS_TO_DISTRIBUTE
+    } BEETS to gauge emissions for the period of ${moment(process.env.VOTE_END_DAY, 'YYYY-MM-DD')
+        .add(DAYS_DELTA_BETWEEN_VOTE_END_AND_EMISSION_START, 'days')
+        .format('MMMM Do YYYY')} to ${moment(process.env.VOTE_END_DAY, 'YYYY-MM-DD')
+        .add(DAYS_DELTA_BETWEEN_VOTE_END_AND_EMISSION_START, 'days')
+        .add(DAYS_FOR_EMISSIONS, 'days')
+        .format('MMMM Do YYYY')}.
 
 To vote, distribute your voting power among pools. You can vote for as many or as few gauges as you wish. You can also vote as many times as you like, overwriting your previous vote with a new vote. The aggregate distribution of responses will be used to calculate the reward distribution.
 
 Pools may have voting incentives provided by Beets and/or other protocols, please check Beets discord for details.`;
 
-        const proposal = {
-            space,
-            type: 'weighted' as ProposalType,
-            title,
-            body,
-            discussion: '',
-            choices: choiceList.map((gauge: GaugeChoice) => gauge.poolName),
-            start: startTimestamp,
-            end: endTimestamp,
-            snapshot: Number(snapshotBlock),
-            network: '146',
-            strategies: JSON.stringify({}),
-            plugins: JSON.stringify({}),
-            metadata: JSON.stringify({}),
-        };
-        console.log('Proposal:', JSON.stringify(proposal));
+    const proposal = {
+        space,
+        type: 'weighted' as ProposalType,
+        title,
+        body,
+        discussion: '',
+        choices: choiceList.map((gauge: GaugeChoice) => gauge.poolName),
+        start: startTimestamp,
+        end: endTimestamp,
+        snapshot: Number(snapshotBlock),
+        network: '146',
+        strategies: JSON.stringify({}),
+        plugins: JSON.stringify({}),
+        metadata: JSON.stringify({}),
+    };
+    console.log('Proposal:', JSON.stringify(proposal));
 
-        let wallet = new Wallet(process.env.PROPOSER_WALLET!);
-
+    let wallet = new Wallet(process.env.PROPOSER_WALLET!);
+    try {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         await client.proposal(wallet, wallet.address, proposal);
     } catch (error) {
-        if (error instanceof Error) core.setFailed(error.message);
+        console.log('Submitting failed');
+        console.log(error);
+        throw error;
     }
 }
+
 run();
