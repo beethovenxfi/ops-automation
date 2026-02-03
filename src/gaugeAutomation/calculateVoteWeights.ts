f5986cd5ab24d2d42b51ae78a163eb6cb81394c2
franz <franzns11@gmail.com>
2026-02-03 09:33:39 +0100

    build transaction batches

--- src/gaugeAutomation/calculateVoteWeights.ts
+++ src/gaugeAutomation/calculateVoteWeights.ts
@@ -3,6 +3,10 @@ import * as fs from 'fs';
 import snapshot from '@snapshot-labs/snapshot.js';
 import { VOTE_WEIGHTS_CSV_PATH } from '../helpers/constants';
 import path from 'path/win32';
+import { createTxnBatchForBountyDisperse, DisperseBountiesTxnInput } from '../helpers/safeCreateJsonBatch';
+import { parseUnits } from 'viem';
+import { readVoteWeightsFromCSV, VoteWeightsDataRow } from '../helpers/csvHelper';
+import { BountyData, readBountiesFromGoogleSheet } from '../helpers/googleSheetHelper';
 
 interface Choice {
     [id: string]: number;
@@ -217,6 +221,118 @@ async function run(): Promise<void> {
         }
         fullFileStream.end();
         console.log(`Done. Exported results to ${filenameFull}`);
+        console.log(`Calculating bounties for disperse now...`);
+
+        // Read vote weights data from CSV
+        const voteWeightDataRows: VoteWeightsDataRow[] = await readVoteWeightsFromCSV();
+
+        // Read bounties from google sheet
+        const bountyDataRows: BountyData[] = await readBountiesFromGoogleSheet();
+
+        // Convert CSV data to transaction inputs
+        const recipientData: {
+            [voterAddress: string]: {
+                [bountyToken: string]: bigint;
+            };
+        } = {};
+
+        const totalBountyAmountsDisperse: { [bountyToken: string]: bigint } = {};
+
+        for (const voteWeightRow of voteWeightDataRows) {
+            const bounties = bountyDataRows.filter((bounty) => bounty.poolTokenName === voteWeightRow.poolName);
+            if (bounties.length === 0) continue; // no bounty for this pool
+            for (const bounty of bounties) {
+                const voterAddress = voteWeightRow.voterAddress.toLowerCase();
+                const bountyToken = bounty.bountyTokenAddress;
+                const voterBountyAmount = (
+                    parseFloat(bounty.bountyAmount) * parseFloat(voteWeightRow.shareVote)
+                ).toFixed(18);
+                const voterBountyAmountWei = parseUnits(
+                    voterBountyAmount.toString(),
+                    parseFloat(bounty.bountyTokenDecimals),
+                );
+                if (!recipientData[voterAddress]) {
+                    recipientData[voterAddress] = {};
+                }
+                if (!recipientData[voterAddress][bountyToken]) {
+                    recipientData[voterAddress][bountyToken] = 0n;
+                }
+                recipientData[voterAddress][bountyToken] += voterBountyAmountWei;
+
+                if (!totalBountyAmountsDisperse[bountyToken]) {
+                    totalBountyAmountsDisperse[bountyToken] = 0n;
+                }
+                totalBountyAmountsDisperse[bountyToken] += voterBountyAmountWei;
+            }
+        }
+        const totalBountyAmountsSheet: { [bountyToken: string]: bigint } = {};
+        for (const bounty of bountyDataRows) {
+            const bountyToken = bounty.bountyTokenAddress;
+            const bountyAmountWei = parseUnits(bounty.bountyAmount.toString(), parseFloat(bounty.bountyTokenDecimals));
+            totalBountyAmountsSheet[bountyToken] = (totalBountyAmountsSheet[bountyToken] || 0n) + bountyAmountWei;
+        }
+
+        console.log(`Found ${Object.keys(recipientData).length} recipients for bounty disperse.`);
+        console.log(`Total bounty tokens to disperse: ${Object.keys(totalBountyAmountsDisperse).length}`);
+        console.log(`Total bounty tokens in sheet: ${Object.keys(totalBountyAmountsSheet).length}`);
+
+        // verify totals match
+        for (const bountyToken in totalBountyAmountsSheet) {
+            const sheetAmount = totalBountyAmountsSheet[bountyToken] || 0n;
+            const disperseAmount = totalBountyAmountsDisperse[bountyToken] || 0n;
+            if (sheetAmount !== disperseAmount) {
+                //correct a user amounts if needed
+                if (sheetAmount > disperseAmount) {
+                    const diff = sheetAmount - disperseAmount;
+
+                    const firstVoter = Object.keys(recipientData)[0];
+                    recipientData[firstVoter][bountyToken] = (recipientData[firstVoter][bountyToken] || 0n) + diff;
+                    console.log(
+                        `Corrected first voter ${firstVoter} by adding ${diff.toString()} wei of token ${bountyToken}`,
+                    );
+                } else {
+                    const diff = disperseAmount - sheetAmount;
+                    // loop through voters and subtract from first with enough balance
+                    for (const voterAddress in recipientData) {
+                        if ((recipientData[voterAddress][bountyToken] || 0n) >= diff) {
+                            recipientData[voterAddress][bountyToken] =
+                                (recipientData[voterAddress][bountyToken] || 0n) - diff;
+                            console.log(
+                                `Corrected voter ${voterAddress} by subtracting ${diff.toString()} wei of token ${bountyToken} to new total of ${recipientData[voterAddress][bountyToken].toString()}`,
+                            );
+                            break;
+                        }
+                    }
+                }
+            } else {
+                console.log(`Total bounty amount verified for token ${bountyToken}: ${sheetAmount.toString()}`);
+            }
+        }
+
+        console.log(`Saving transaction batches now.`);
+        // Prepare disperse transaction inputs
+        const disperseInputs: DisperseBountiesTxnInput[] = [];
+        for (const voterAddress in recipientData) {
+            for (const bountyToken in recipientData[voterAddress]) {
+                const tokenInput = disperseInputs.find((input) => input.tokenAddress === bountyToken);
+                if (!tokenInput) {
+                    disperseInputs.push({
+                        tokenAddress: bountyToken as `0x${string}`,
+                        recipients: [{ address: voterAddress, amountInWei: recipientData[voterAddress][bountyToken] }],
+                    });
+                } else {
+                    tokenInput.recipients.push({
+                        address: voterAddress,
+                        amountInWei: recipientData[voterAddress][bountyToken],
+                    });
+                }
+            }
+        }
+
+        for (const input of disperseInputs) {
+            createTxnBatchForBountyDisperse(input);
+        }
+        console.log(`All done.`);
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message);
     }
